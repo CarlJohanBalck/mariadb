47de59608ca910230a08249a0735a1ec90fb72f2
Carl Johan Balck <carljohanbalck@carls-macbook.local>
2023-10-29 07:39:50 +0100

    refactoring changes 2

--- push_mariadb_arm.sh
+++ push_mariadb_arm.sh
@@ -7,19 +7,19 @@ source config.sh
 docker exec db /usr/bin/mysqldump -u root --password=root cool_db > backup_${DATE}.sql
 
 # Build the Docker image using the MariaDB base image and the database dump
-docker build --no-cache -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG -f Dockerfile_arm .
+docker build --no-cache -t $DOCKER_IMAGE_NAME_ARM:$DOCKER_IMAGE_TAG -f Dockerfile_arm .
 
 # Login to Docker Hub
 docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
 
 # Tag the Docker image for deployment to Docker Hub
-docker tag $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+docker tag $DOCKER_IMAGE_NAME_ARM:$DOCKER_IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_IMAGE_NAME_ARM:$DOCKER_IMAGE_TAG
 
 # Push the Docker image to Docker Hub
-docker push $DOCKER_HUB_USERNAME/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+docker push $DOCKER_HUB_USERNAME/$DOCKER_IMAGE_NAME_ARM:$DOCKER_IMAGE_TAG
 
 # Remove the local Docker image
-docker rmi $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+docker rmi $DOCKER_IMAGE_NAME_ARM:$DOCKER_IMAGE_TAG
 
 # Remove the database dump fileﬁ
 rm backup_${DATE}.sql
